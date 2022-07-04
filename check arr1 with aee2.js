57619eec22cca93448c21f79ca4feb2fb185edb3
Mauro Busso <107254152+maurobusso@users.noreply.github.com>
2022-07-04 20:06:35 +0100

    Add files via upload
    The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
    
    The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
    
    If the score < 0, return 0.

---
+++ check arr1 with aee2.js
@@ -0,0 +1,36 @@
+function checkExam(array1, array2) {
+    var score = 0;
+      
+      for (var i = 0; i < array1.length; i++){
+      
+        if (array1[i] == array2[i]) {
+        
+        score += 4;
+        
+        }
+        
+        else if (array2[i] === ""){
+    
+          score += 0
+      
+        }
+        
+         
+        else {
+        
+        score -= 1
+        
+        }
+    
+      
+      }
+      
+      if (score < 0) {
+      
+        score = 0
+        
+      }  
+     
+     return score
+     
+    }
